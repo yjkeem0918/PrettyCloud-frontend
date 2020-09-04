6b15cbbede3f82d2ed4be06ccd93f033a8d9ab16
yoonminsohappy@gmail.com <yoonminsohappy@gmail.com>
2020-09-04 12:53:18 +0900

    Modify: 레이아웃 리뷰 반영

--- src/Pages/SearchResult/SearchResult.js
+++ src/Pages/SearchResult/SearchResult.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
 import "./SearchResult.scss";
 
+const OPTIONS = [
+  { id: "1", type: "공간 유형", option: "모든 공간", icon: "downArrow" },
+  { id: "2", type: "지역", option: "전체", icon: "downArrow" },
+  { id: "3", type: "이용일", option: "모든 날짜", icon: "calendarIcon" },
+];
+
+const CATEGORY = [
+  { list: "전체", bar: true },
+  { list: "시간단위", bar: true },
+  { list: "패키지단위", bar: true },
+  { list: "월단위", bar: false },
+];
+
+const SELECT = [
+  { value: "first", order: "베스트 공간 순" },
+  { value: "second", order: "가격 낮은 순" },
+  { value: "third", order: "가격 높은 순" },
+  { value: "fourth", order: "이용후기 많은 순" },
+];
+
+const BUTTON = [
+  { id: "1", icon: "locationIcon", name: "지도" },
+  { id: "2", icon: "filterIcon", name: "필터" },
+];
+
 class SearchResult extends Component {
   render() {
     return (
@@ -10,69 +35,57 @@ class SearchResult extends Component {
           <span className="resultText">(으)로 검색한 결과입니다.</span>
           <div className="selectBarContainer">
             <div className="optionContainer">
-              <div className="optionBox">
-                <span className="optionType">공간 유형</span>
-                <div className="optionWrapper">
-                  <span className="option">모든 공간</span>
-                  <div className="downArrow"></div>
-                </div>
-              </div>
-              <div className="optionBox">
-                <span className="optionType">지역</span>
-                <div className="optionWrapper">
-                  <span className="option">전체</span>
-                  <div className="downArrow"></div>
-                </div>
-              </div>
-              <div className="optionBox">
-                <span className="optionType">이용일</span>
-                <div className="optionWrapper">
-                  <span className="option">모든 날짜</span>
-                  <div className="calendarIcon"></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="buttonContainer">
-              <div className="buttonBox">
-                <div className="locationIcon"></div>
-                <button className="button">지도</button>
-              </div>
-              <div className="buttonBox">
-                <div className="filterIcon"></div>
-                <button className="button">필터</button>
+              {OPTIONS.map((el) => {
+                return (
+                  <div className="optionBox" key={el.id}>
+                    <span className="optionType">{el.type}</span>
+                    <div className="optionWrapper">
+                      <span className="option">{el.option}</span>
+                      <div className={el.icon} />
+                    </div>
+                  </div>
+                );
+              })}
+              <div className="buttonContainer">
+                {BUTTON.map((el) => {
+                  return (
+                    <div className="buttonBox" key={el.id}>
+                      <div className={el.icon} />
+                      <button className="button">{el.name}</button>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="categoryBarContainer">
-          <div className="categoryBox">
-            <ul className="categoryList">
-              <li>전체 </li>
-              <div className="categoryBar"></div>
-              <li>시간단위</li>
-              <div className="categoryBar"></div>
-              <li>패키지단위</li>
-              <div className="categoryBar"></div>
-              <li>월단위</li>
-            </ul>
-
-            <div className="checkBoxContainer">
-              <input className="checkingDiscount" type="checkbox" />
-              <span className="discountSpace">지금 쿠폰 할인되는 공간</span>
-              <span className="newTag">NEW</span>
+          <div className="categoryBarContainer">
+            <div className="categoryBox">
+              <ul className="categoryList">
+                {CATEGORY.map((el) => {
+                  return (
+                    <>
+                      <li>{el.list}</li>
+                      <div className={el.bar ? "categoryBar" : ""} />
+                    </>
+                  );
+                })}
+              </ul>
+              <div className="checkBoxContainer">
+                <input className="checkingDiscount" type="checkbox" />
+                <span className="discountSpace">지금 쿠폰 할인되는 공간</span>
+                <span className="newTag">NEW</span>
+              </div>
             </div>
+            <select>
+              {SELECT.map((el) => {
+                return (
+                  <>
+                    <option value={el.value}>{el.order}</option>
+                  </>
+                );
+              })}
+            </select>
           </div>
-
-          <select>
-            <option value="first" selected>
-              베스트 공간 순
-            </option>
-            <option value="second">가격 낮은 순</option>
-            <option value="third">가격 높은 순</option>
-            <option value="fourth">이용후기 많은 순</option>
-          </select>
         </div>
       </div>
     );
