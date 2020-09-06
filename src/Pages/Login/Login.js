6f501e7194264814b88b8b0e213577ed8a747627
김영지 <yjkim7257@gmail.com>
2020-09-07 05:06:39 +0900

    Add: 로그인 회원가입 기능추가

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,16 +3,34 @@ import { Link } from "react-router-dom";
 import "./Login.scss";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      alertEmail: true,
+    };
+  }
+
+  handleEmail = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  checkEmail = () => {
+    this.setState({
+      alertEmail: this.state.email.includes("@" && ".com") ? true : false,
+    });
+  };
   render() {
     return (
       <div className="Login">
-          <main>
-            <h1>로그인</h1>
-            <div className="loginContainer"> 
+        <main>
+          <h1>로그인</h1>
+          <div className="loginContainer">
             <div className="socialLoginBox">
-              <a href="#">
-                네이버로 로그인
-              </a>
+              <a href="#">네이버로 로그인</a>
               <a href="#" className="kakao">
                 카카오로 로그인
               </a>
@@ -24,27 +42,42 @@ class Login extends Component {
             </div>
             <section>
               <div className="inputBox">
-                <input placeholder="이메일"/>
-                <input className="password" type="password" placeholder="비밀번호" />
+                <input
+                  placeholder="이메일"
+                  onChange={this.handleEmail}
+                  onKeyUp={this.checkEmail}
+                />
+                <div
+                  className={this.state.alertEmail ? "hiddenAlert" : "alert"}
+                >
+                  이메일 형식이 유효하지 않습니다.
+                </div>
+                <input
+                  className="password"
+                  type="password"
+                  placeholder="비밀번호"
+                />
               </div>
               <div className="buttonBox">
                 <div className="memory">
-                  <input type="checkbox" id="a1" name="로그인기억"/>
+                  <input type="checkbox" id="a1" name="로그인기억" />
                   <label for="a1"> </label>
                   <span>로그인 기억하기</span>
                 </div>
                 <a>비밀번호 찾기</a>
               </div>
+              <Link to="/main">
               <button>이메일로 로그인</button>
+              </Link>
               <p>
                 아직 스페이스클라우드 회원이 아니신가요?
                 <Link to="/signUp">
-                 <a href="#">회원가입</a>
+                  <a href="#">회원가입</a>
                 </Link>
               </p>
             </section>
-            </div>
-          </main>
+          </div>
+        </main>
       </div>
     );
   }
