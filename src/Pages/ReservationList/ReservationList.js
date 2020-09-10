15a593f677de8ecbacbefba84bbd630c3f2add12
김영지 <yjkim7257@gmail.com>
2020-09-11 07:19:11 +0900

    Add: 예약리스트 Api

--- src/Pages/ReservationList/ReservationList.js
+++ src/Pages/ReservationList/ReservationList.js
@@ -11,16 +11,26 @@ class ReservationList extends Component {
   }
 
   componentDidMount = () => {
-    fetch("http://localhost:3000/data/reservation.json")
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({
-          reservation: res.reservation,
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch("http://192.168.219.108:8001/reservation/get", {
+        method: "GET",
+        headers: {
+          authorization: token,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          this.setState({
+            reservation: res.data[0],
+          });
         });
-      });
+    }
   };
 
   render() {
+    console.log(this.state.reservation);
     return (
       <div className="ReservationList">
         <h1>예약 내역 리스트</h1>
@@ -30,12 +40,11 @@ class ReservationList extends Component {
               {this.state.reservation.map((el, idx) => {
                 return (
                   <Reservation
-                    name={el.name}
-                    date={el.date}
-                    time={el.time}
-                    totalTime={el.totalTime}
-                    person={el.person}
-                    price={el.price}
+                    name={el.title}
+                    time={`${el.start_time}~${el.end_time}`}
+                    totalTime={el.end_time - el.start_time}
+                    person={el.people}
+                    price={parseInt(el.total_fee).toLocaleString()}
                     key={idx}
                   />
                 );
